0ece004153d0e5e6cae1db5617058dea4750249c
Patrick Mataba <matabapatrick@outlook.ph>
2021-02-28 00:09:04 +0800

    route update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('hello world | hi world push to dev s');
+    return view('hello world | hi world push to dev s | hello world track test');
 });
