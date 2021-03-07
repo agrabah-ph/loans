ab000cec241e94f172269eab80216388da4bbcd0
Patrick Mataba <matabapatrick@outlook.ph>
2021-03-07 22:06:15 +0800

    feature: email verification on signup added route: added new middleware for verified users update: .env file on .gitignore for a reason that the app may get some error due to different configurations update: new mailtrap configs on .env

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,6 +18,6 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
