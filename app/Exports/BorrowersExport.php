f6f94e50997c55c6ce00559ffe6d8dd6be86f12f
frightnight <jasper.garcera@gmail.com>
2021-08-24 03:29:55 +0800

    borrowers data export

---
+++ app/Exports/BorrowersExport.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Exports;
+
+use App\Loan;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class BorrowersExport implements FromCollection, WithMapping, WithHeadings
+{
+    protected $status;
+
+    function __construct($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Loan::with('borrower')->get();
+    }
+
+    /**
+     * @param mixed $borrower
+     * @return array
+     */
+    public function map($data): array
+    {
+        return [
+            $this->status,
+            $data->borrower->profile->first_name,
+            $data->borrower->profile->middle_name,
+            $data->borrower->profile->last_name,
+            $data->borrower->profile->dob,
+            $data->borrower->profile->civil_status,
+            $data->borrower->profile->gender,
+            $data->borrower->profile->landline,
+            $data->borrower->profile->mobile,
+            $data->borrower->profile->tin,
+            $data->borrower->profile->sss_gsis,
+            $data->borrower->profile->education,
+            $data->borrower->profile->secondary_info[0][2],
+            $data->borrower->profile->secondary_info[1][2],
+            $data->borrower->profile->secondary_info[2][2],
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Status',
+            'First Name',
+            'Middle Name',
+            'Last Name',
+            'Date of Birth',
+            'Civil Status',
+            'Gender',
+            'Land line',
+            'Mobile',
+            'TIN ID#',
+            'SSS/GSIS #',
+            'Educational Attainment',
+            'Current Address',
+            'Years of Stay',
+            'Address Status',
+        ];
+    }
+}
--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\CommunityLeader;
 use App\Events\NewUserRegisteredEvent;
+use App\Exports\BorrowersExport;
 use App\Exports\FarmersExport;
 use App\Farmer;
 use App\Inventory;
@@ -309,12 +310,14 @@ class PublicController extends Controller
 
     public function export()
     {
-        return Excel::download(new FarmersExport(), 'farmers.xlsx');
+        $status = 'Active';
+        return Excel::download(new BorrowersExport($status), 'borrowers.html');
     }
 
     public function test()
     {
-        $loans = Loan::where('status', 'Active')->get();
+//        $loans = Loan::where('status', 'Active')->get();
+        $loans = Loan::with('borrower')->get();
 //        smsNotification('loan-due', 1);
         return $loans;
     }
